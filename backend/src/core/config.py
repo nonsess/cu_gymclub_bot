8b5e0bf9372a1a0c7718b930d4c5490401fbaf61
Daniil <petrovdanil230@gmail.com>
2026-02-23 20:02:44 +0300

    feat(notifications)

--- backend/src/core/config.py
+++ backend/src/core/config.py
@@ -5,6 +5,8 @@ class Settings(BaseSettings):
     POSTGRES_PASSWORD: str
     POSTGRES_DB: str
 
+    TELEGRAM_BOT_TOKEN: str
+
     @property
     def POSTGRES_DSN(self):
         return f"postgresql+asyncpg://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}@db:5432/{self.POSTGRES_DB}"
