7921498d544dd292e1752b24311f187274837872
Daniil <petrovdanil230@gmail.com>
2026-02-25 16:08:51 +0300

    del(extra): del extra exception

--- backend/src/core/exceptions/action.py
+++ backend/src/core/exceptions/action.py
@@ -1,11 +1,4 @@
-from .base import ConflictException, ValidationException
-
-class ActionAlreadyExistsException(ConflictException):
-    def __init__(self, from_user_id: int, to_user_id: int):
-        super().__init__(
-            f"Action from user {from_user_id} to user {to_user_id} already exists",
-            "ACTION_ALREADY_EXISTS"
-        )
+from .base import ValidationException
 
 class SelfActionException(ValidationException):
     def __init__(self):
